b3e3b3056e3e067f40f67084d09ead9d0d20b498
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2022-07-03 16:57:08 -0300

    Add initial files

---
+++ app/src/main/java/com/test/udacity_nd940_custom_download/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.udacity_nd940_custom_download
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
